26648c3bdedb951b24e48bcbb4d2567e0183680c
l3ond3v <leonbravo81@gmail.com>
2025-07-31 17:43:09 -0500

    Aplicación y dependiencias listas

---
+++ Requerimientos/Reto_Nacional2023/WSC_Session1_LoginApp/WSC_Session1_LoginApp/SeoulStayLogin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WSC_Session1_LoginApp
+{
+    public partial class SeoulStayLogin : Form
+    {
+        public SeoulStayLogin()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Requerimientos/Reto_Nacional2023/WSC_Session1_LoginApp2/AppRetoSession1/AppRetoSession1/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MySql.Data.MySqlClient;
+
+namespace AppRetoSession1
+{
+    public class Conexion
+    {
+        private string cadenaConexion = "server=localhost;database=sesion1;uid=root;pwd=12345;";
+
+        public MySqlConnection ObtenerConexion()
+        {
+            MySqlConnection conexion = new MySqlConnection(cadenaConexion);
+            conexion.Open();
+            return conexion;
+        }
+    }
+}
